dc7247c4accd6e926b21bea976890bbb0d7c0028
safawat hossain <sifakatali@gmail.com>
2021-02-07 15:13:37 +0600

    secpo0nd part primary

--- apps.js
+++ apps.js
@@ -1,20 +1,19 @@
 
-
 const foodItem = document.getElementById('foodItem');
 const itemList = document.querySelectorAll('.itemList');
 
 document.getElementById('searchFood').addEventListener('click', function(){
     const food = document.getElementById('foodName');
     const foodName = food.value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/filter.php?i=${foodName}`)
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
     .then(res => res.json())
-    .then(data => { console.log(data);
-        let html="";
+    .then(data => {
+        let foodMeal="";
         if(data.meals){
             data.meals.forEach(meals => {
-             html +=` 
-                <div class="col-md-3 mb-3">
-                <div class="card h-100  shadow  p-3 ">
+              foodMeal +=` 
+                <div class="col-md-3 mb-3" onclick="food(${meals.idMeal});">
+                <div class="card h-100  shadow  p-3" >
                   <img src="${meals.strMealThumb}" class="card-img-top">
                   <div class="card-body">
                     <h5 class="card-title fw-bold">${meals.strMeal}</h5>
@@ -23,7 +22,44 @@ document.getElementById('searchFood').addEventListener('click', function(){
               </div>
                 `  
             });
-        } foodItem.innerHTML=html;  
-        
+        }else{} 
+        foodItem.innerHTML=foodMeal;  
     } )
+    .catch(error => {
+        let foodMeal =`
+        <div class="col-md-3 mb-3">
+        <div class="card h-100  shadow  p-3 ">
+          <h1> result not found</h1>
+        </div>
+      </div>
+        `
+        foodItem.innerHTML=foodMeal; 
+
+    })
+
+
 })
+
+function food(idMeal){
+  const mealId = idMeal;
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+  .then(res=> res.json())
+  .then(data =>{
+
+     console.log(data);
+     const foodObject = data.meals[0];
+     console.log(foodObject);
+     const mealItem1 =  foodObject.strIngredient1;
+     const mealItem2 =  foodObject.strIngredient2;
+     const mealItem3 =  foodObject.strIngredient3;
+     const mealItem4 =  foodObject.strIngredient4;
+     const mealItem5 =  foodObject.strIngredient5;
+     const mealItem6 =  foodObject.strIngredient6;
+     console.log(mealItem1);
+     console.log(mealItem2);
+     console.log(mealItem3);
+     console.log(mealItem4);
+     console.log(mealItem5);
+     console.log(mealItem6);
+  })
+}
