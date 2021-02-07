2cc97acc75dd09d4f8e392c07b59774516100e2c
safawat hossain <sifakatali@gmail.com>
2021-02-07 18:50:22 +0600

    this is last final

--- apps.js
+++ apps.js
@@ -9,55 +9,62 @@ const itemList = document.querySelectorAll('.itemList');
 document.getElementById('searchFood').addEventListener('click', function(){
     const food = document.getElementById('foodName');
     const foodName = food.value;
-    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
-    .then(res => res.json())
-    .then(data => {
-        let foodMeal="";
-        if(data.meals){
-            data.meals.forEach(meals => {
-              foodMeal +=` 
-                <div class="col-md-3 mb-3" onclick="food(${meals.idMeal});">
-                <div class="card h-100  shadow  p-3" >
-                  <img src="${meals.strMealThumb}" class="card-img-top">
-                  <div class="card-body">
-                    <h5 class="card-title fw-bold">${meals.strMeal}</h5>
+    if(foodName == ''){
+      alert('Please Enter First Letter');
+    }else{
+      fetch(`https://www.themealdb.com/api/json/v1/1/search.php?f=${foodName}`)
+      .then(res => res.json())
+      .then(data => {
+          let foodMeal="";
+          if(data.meals){
+              data.meals.forEach(meals => {
+                foodMeal +=` 
+                  <div class="col-md-3 mb-3" onclick="food(${meals.idMeal});">
+                  <div class="card h-100  shadow  p-3" >
+                    <img src="${meals.strMealThumb}" class="card-img-top">
+                    <div class="card-body">
+                      <h5 class="card-title orangeColor fw-bold">${meals.strMeal}</h5>
+                    </div>
                   </div>
                 </div>
-              </div>
-                `  
-            });
-            foodItem.innerHTML=foodMeal; 
-            errorHandel.style.display="none";
-            parentClass.style.display="block";
-
-        }else{
-          foodMeal =`
-           <h3>Meals not find</h3>
+                  ` ; 
+              });
+              foodItem.innerHTML=foodMeal; 
+              errorHandel.style.display="none";
+              parentClass.style.display="block";
+              
+  
+          }else{
+            foodMeal =`
+             <h3>Meals not find</h3>
+            `;
+            errorHandel.innerHTML=foodMeal; 
+            parentClass.style.display="none";
+            errorHandel.style.display="block";
+            foodDetails.style.display="none";
+  
+          } 
+           
+      } )
+      .catch(error => {
+          let foodMeal =`
+            <h3> Search not found.Please Search With First Letter </h3>
           `;
-          errorHandel.innerHTML=foodMeal; 
+          errorHandel.innerHTML = foodMeal;
           parentClass.style.display="none";
           errorHandel.style.display="block";
-
-        } 
-         
-    } )
-    .catch(error => {
-        let foodMeal =`
-          <h3> Search not found.Please Search With First Letter </h3>
-        `
-        errorHandel.innerHTML = foodMeal;
-        parentClass.style.display="none";
-        errorHandel.style.display="block";
- 
-
-    })
+          foodDetails.style.display="none";
+   
+  
+      })
+    }
 
 
 })
 
-function food(idMeal){
+const food=(idMeal)=>{
   const mealId = idMeal;
-  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+   fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
   .then(res=> res.json())
   .then(data =>{
      const foodObject = data.meals[0];
@@ -65,7 +72,7 @@ function food(idMeal){
     <div class="card h-100  shadow card_radius p-3 ">
     <img src="${foodObject.strMealThumb}" class="card-img-top card-bg " alt="...">
     <div class="card-body">
-      <h5 class="card-title fw-bold">${foodObject.strMeal}</h5>
+      <h5 class="card-title orangeColor fw-bold ">${foodObject.strMeal}</h5>
       <h6>Ingredient</h6>
       <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient1} </li>
       <li><span><i class="fas fa-check-square orange"></i> </span> ${foodObject.strIngredient2} </li>
